f18f57206186813f298b6051a48ad241a4228f19
Dmitrii Frolov <steel835@gmail.com>
2023-10-01 15:49:25 +0400

    First launch fixes (launch settings, assembly, required)

--- src/Test.Rabbit.Producer.WebApi/Controllers/Users/Contracts/CreateUser/CreateUserDto.cs
+++ src/Test.Rabbit.Producer.WebApi/Controllers/Users/Contracts/CreateUser/CreateUserDto.cs
@@ -4,7 +4,7 @@ public sealed class CreateUserDto
 {
     public required string FirstName { get; init; }
     public required string LastName { get; init; }
-    public required string? MiddleName { get; init; }
+    public string? MiddleName { get; init; }
     public required string PhoneNumber { get; init; }
     public required string EmailAddress { get; init; }
 }
